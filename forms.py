1b68171318d157b640e9655cd9566752496d0d8b
Wilco de Boer <deboer.wilco@gmail.com>
2021-04-11 04:20:07 +0200

    Improve artist field

--- forms.py
+++ forms.py
@@ -9,7 +9,7 @@ class QueryForm(FlaskForm):
 
 
 class AudioEditForm(FlaskForm):
-    artist = FieldList(StringField(validators=[InputRequired()]), label='Artists', min_entries=1)
+    artist = FieldList(StringField(label='Artist', validators=[InputRequired()]), min_entries=1)
     title = StringField(label='Title', validators=[InputRequired()])
     album = StringField(label='Album')
     genre = StringField(label='Genre')
